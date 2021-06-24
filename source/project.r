74ada73c55341eb394e589c482886ec3a9bad241
morm24 <74315267+morm24@users.noreply.github.com>
2021-06-24 23:06:15 +0200

    Grundstruktur aufgebaut

---
+++ source/project.r
@@ -0,0 +1,8 @@
+pacman::p_load(shiny,janitor, tidyverse, broom)
+
+emmission <- read_csv("C:/Users/Moritz/Desktop/DBS-Projekt/data/co2_emission.csv")
+gdp <- read_csv("C:/Users/Moritz/Desktop/DBS-Projekt/data/gdp.csv")
+p_growth <- read_csv("C:/Users/Moritz/Desktop/DBS-Projekt/data/population_growth.csv")
+p_total <- read_csv("C:/Users/Moritz/Desktop/DBS-Projekt/data/population_total.csv")
+energy <- read_csv("C:/Users/Moritz/Desktop/DBS-Projekt/data/global-energy-substitution.csv")
+
